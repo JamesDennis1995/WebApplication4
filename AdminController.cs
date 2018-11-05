31a95e85f8e1f51398671dfeefd083adef3953d5
JamesDennis1995 <44235413+JamesDennis1995@users.noreply.github.com>
2018-11-05 14:43:53 +0000

    Add files via upload

---
+++ AdminController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public ActionResult AdminLogin()
+        {
+            var db = new JamesEntities();
+            var check = db.AdminPassword.Where(u => u.Id > 0).FirstOrDefault();
+            if (check == null)
+            {
+                var adminPassword = Salt.Encode("Pa$$w0rd", null);
+                db.AdminPassword.Add(new AdminPassword
+                {
+                    AdminPassword1 = adminPassword
+                });
+                db.SaveChanges();
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AdminLogin(AdminPassword model)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new JamesEntities();
+                var password = model.AdminPassword1;
+                var check = db.AdminPassword.Where(u => u.Id > 0).FirstOrDefault();
+                bool correct = Salt.Verify(password, check.AdminPassword1);
+                if (correct == true)
+                {
+                    Session["Message"] = "Welcome.";
+                    Session["AdminUsername"] = "Admin";
+                    return RedirectToAction("Admin", "Admin");
+                }
+                else
+                {
+                    ViewData["Message"] = "Login failed.";
+                }
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Admin()
+        {
+            if (Session["AdminUsername"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("AdminLogin", "Admin");
+            }
+        }
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("AdminLogin", "Admin");
+        }
+        [HttpGet]
+        public ActionResult AddStock()
+        {
+            if (Session["AdminUsername"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("AdminLogin", "Admin");
+            }
+        }
+        [HttpPost]
+        public JsonResult StockCodeCheck(string check)
+        {
+            var db = new JamesEntities();
+            var duplicateCheck = db.Stock.Where(u => u.Code == check).FirstOrDefault();
+            if (duplicateCheck == null)
+            {
+                return Json(new { free = true });
+            }
+            else
+            {
+                return Json(new { free = false });
+            }
+        }
+        [HttpPost]
+        public ActionResult AddStock(Stock model, HttpPostedFileBase image)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new JamesEntities();
+                db.Stock.Add(new Stock
+                {
+                    Code = model.Code,
+                    Description = model.Description,
+                    PricePerUnit = model.PricePerUnit
+                });
+                db.SaveChanges();
+                var location = Path.Combine(Server.MapPath("~/Images/"), model.Code + ".jpg");
+                image.SaveAs(location);
+                Session["Message"] = "Stock item successfully added.";
+                return RedirectToAction("Admin", "Admin");
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult ChangeAdminPassword()
+        {
+            if (Session["AdminUsername"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("AdminLogin", "Admin");
+            }
+        }
+        [HttpPost]
+        public ActionResult ChangeAdminPassword(AdminPassword model, string OldPassword, string NewPassword, string ConfirmNewPassword)
+        {
+            if (ModelState.IsValid)
+            {
+                if (NewPassword != ConfirmNewPassword)
+                {
+                    ViewData["Message"] = "New Password and Confirm New Password do not match.";
+                }
+                else
+                {
+                    var db = new JamesEntities();
+                    var check = db.AdminPassword.Where(u => u.Id > 0).FirstOrDefault();
+                    bool correct = Salt.Verify(OldPassword, check.AdminPassword1);
+                    if (correct == false)
+                    {
+                        ViewData["Message"] = "Old Password is incorrect.";
+                    }
+                    else
+                    {
+                        check.AdminPassword1 = Salt.Encode(NewPassword, null);
+                        db.SaveChanges();
+                        Session["Message"] = "Admin password successfully changed. Please notify other admins.";
+                        return RedirectToAction("Admin", "Admin");
+                    }
+                }
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult CheckOrders()
+        {
+            if (Session["AdminUsername"] != null)
+            {
+                var db = new JamesEntities();
+                var list = db.Order.ToList();
+                List<string> orderNumbers = new List<string>();
+                orderNumbers.Add("");
+                for (int i = 0; i < list.Count; i++)
+                {
+                    orderNumbers.Add(list[i].Id.ToString());
+                }
+                SelectList orderNumbers1 = new SelectList(orderNumbers);
+                ViewData["OrderNumbers"] = orderNumbers1;
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("AdminLogin", "Admin");
+            }
+        }
+        [HttpPost]
+        public JsonResult GetOrderInfo(string orderNumber)
+        {
+            var orderNumber2 = int.Parse(orderNumber);
+            var db = new JamesEntities();
+            var details = db.OrderStock.Join(db.Stock, os => os.StockCode, s => s.Code, (os, s) => new { os, s }).Join(db.Order, oss => oss.os.OrderID, o => o.Id, (oss, o) => new { oss, o }).Join(db.Customer, osso => osso.o.Customer, c => c.Id, (osso, c) => new { osso.o.Id, c.FirstName, c.Surname, c.ContactNumber, c.Address1, c.Address2,  c.TownCity, c.County, c.Postcode, osso.oss.s.Code, osso.oss.s.Description, osso.oss.os.Quantity, Total = osso.o.OrderTotal }).Where(u => u.Id == orderNumber2).ToList();
+            return Json(details);
+        }
+    }
+    }
---
+++ UserController.cs
@@ -0,0 +1,277 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult EmailCheck(string check)
+        {
+            var db = new JamesEntities();
+            var duplicateCheck = db.Customer.Where(u => u.Email == check).FirstOrDefault();
+            if (duplicateCheck == null)
+            {
+                return Json(new { free = true });
+            }
+            else
+            {
+                return Json(new { free = false });
+            }
+        }
+        [HttpPost]
+        public ActionResult CreateAccount(Customer model)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new JamesEntities();
+                db.Customer.Add(new Customer
+                {
+                    FirstName = model.FirstName,
+                    Surname = model.Surname,
+                    Password = Salt.Encode(model.Password, null),
+                    ContactNumber = model.ContactNumber,
+                    Email = model.Email,
+                    Address1 = model.Address1,
+                    Address2 = model.Address2,
+                    TownCity = model.TownCity,
+                    County = model.County,
+                    Postcode = model.Postcode
+                });
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Customer model)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new JamesEntities();
+                var password = model.Password;
+                var check = db.Customer.Where(u => u.Email == model.Email).FirstOrDefault();
+                if (check == null)
+                {
+                    ViewData["Message"] = "Email address not found.";
+                }
+                else
+                {
+                    bool correct = Salt.Verify(password, check.Password);
+                    if (correct == true)
+                    {
+                        Session["Name"] = check.FirstName;
+                        Session["Email"] = check.Email;
+                        Session["Message"] = "Welcome, " + check.FirstName + ".";
+                        return RedirectToAction("Customer", "User");
+                    }
+                    else
+                    {
+                        ViewData["Message"] = "Login failed.";
+                    }
+                }
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Customer()
+        {
+            if (Session["Email"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+        }
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "User");
+        }
+        [HttpGet]
+        public ActionResult EditDetails()
+        {
+            if (Session["Email"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+        }
+        [HttpPost]
+        public ActionResult EditDetails(Customer model, bool Blank)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new JamesEntities();
+                var search = Session["Email"];
+                var toUpdate = db.Customer.Where(u => u.Email == search).FirstOrDefault();
+                    if (model.FirstName != null)
+                    {
+                        toUpdate.FirstName = model.FirstName;
+                    }
+                    if (model.Surname != null)
+                    {
+                        toUpdate.Surname = model.Surname;
+                    }
+                    if (model.Password != null)
+                    {
+                        toUpdate.Password = Salt.Encode(model.Password, null);
+                    }
+                    if (model.ContactNumber != null)
+                    {
+                        toUpdate.ContactNumber = model.ContactNumber;
+                    }
+                    if (model.Email != null)
+                    {
+                        Session["Email"] = model.Email;
+                        toUpdate.Email = model.Email;
+                    }
+                    if (model.Address1 != null)
+                    {
+                        toUpdate.Address1 = model.Address1;
+                    }
+                    if (model.Address2 != null || Blank == true)
+                    {
+                        toUpdate.Address2 = model.Address2;
+                    }
+                    if (model.TownCity != null)
+                    {
+                        toUpdate.TownCity = model.TownCity;
+                    }
+                    if (model.County != null)
+                    {
+                        toUpdate.County = model.County;
+                    }
+                    if (model.Postcode != null)
+                    {
+                        toUpdate.Postcode = model.Postcode;
+                    }
+                    db.SaveChanges();
+                    Session["Message"] = "Your details have been successfully updated.";
+                    return RedirectToAction("Customer", "User");      
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Order()
+        {
+            if (Session["Email"] != null)
+            {
+                var db = new JamesEntities();
+                var list = db.Stock.ToList();
+                List<string> codes = new List<string>();
+                codes.Add("");
+                for (int i = 0; i < list.Count; i++)
+                {
+                    codes.Add(list[i].Code);
+                }
+                SelectList codes1 = new SelectList(codes);
+                ViewData["StockCodes"] = codes1;
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+        }
+        [HttpPost]
+        public JsonResult GetDetails(string stockCode)
+        {
+                var db = new JamesEntities();
+                var item = db.Stock.Select(u => new { u.Code, u.Description, u.PricePerUnit }).Where(v => v.Code == stockCode).FirstOrDefault();
+                return Json(item, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Order(IList<Item> Items)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new JamesEntities();
+                var numberOfItems = Items.Count;
+                var orderNumber = db.Order.ToList().Count + 1;
+                var combinedTotal = 0.0;
+                var search = Session["Email"];
+                var userID = db.Customer.Where(u => u.Email == search).FirstOrDefault().Id;
+                for (int i = 0; i < numberOfItems; i++)
+                {
+                    combinedTotal += Items[i].Total;
+                }
+                combinedTotal = Math.Round(combinedTotal, 2);
+                db.Order.Add(new Order
+                {
+                    Customer = userID,
+                    OrderTotal = combinedTotal
+                });
+                db.SaveChanges();
+                for (int j = 0; j < numberOfItems; j++)
+                {
+                    db.OrderStock.Add(new OrderStock
+                    {
+                        OrderID = orderNumber,
+                        StockCode = Items[j].Code,
+                        Quantity = Items[j].Quantity
+                });
+                }
+                db.SaveChanges();
+                Session["Message"] = "Order successfully placed.";
+                return RedirectToAction("Customer", "User");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CheckOrders()
+        {
+            if (Session["Email"] != null)
+            {
+                var db = new JamesEntities();
+                var search = Session["Email"];
+                var check = db.Customer.Where(u => u.Email == search).FirstOrDefault();
+                var userID = check.Id;
+                var list = db.Order.Where(u => u.Customer == userID).ToList();
+                List<string> orderNumbers = new List<string>();
+                orderNumbers.Add("");
+                for (int i = 0; i < list.Count; i++)
+                {
+                    orderNumbers.Add(list[i].Id.ToString());
+                }
+                SelectList orderNumbers1 = new SelectList(orderNumbers);
+                ViewData["OrderNumbers"] = orderNumbers1;
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+        }
+        [HttpPost]
+        public JsonResult GetOrderInfo (string orderNumber)
+        {
+            var orderNumber2 = int.Parse(orderNumber);
+            var db = new JamesEntities();
+            var details = db.OrderStock.Join(db.Order, os => os.OrderID, o => o.Id, (os, o) => new { os, o }).Join(db.Stock, oso => oso.os.StockCode, s => s.Code, (oso, s) => new { Id = oso.os.OrderID, oso.os.Quantity, s.Code, s.Description, Total = oso.o.OrderTotal}).Where(u => u.Id == orderNumber2).ToList();
+            return Json(details);
+        }
+    }
+}
